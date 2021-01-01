7791bf5385325f9be874d73d835a6d1757cbaf38
Alex Silver <alex.silver@intel.com>
2021-01-01 15:47:35 -0800

    Finalized color and styles

--- src/components/BottomPill.js
+++ src/components/BottomPill.js
@@ -3,7 +3,7 @@ import React from 'react';
 // BottomPill is the bottom half of the pill card.
 function BottomPill(props) {
     return (
-        <div className={`${!props.toggled ? "bg-blue-200" : "bg-gray-900"} transition ease-in-out duration-500 rounded-xl rounded-t-none px-4 py-8 w-full shadow-2xl text-center`}>
+        <div className={`${!props.toggled ? "bg-blue-sky" : "bg-gray-deep"} transition ease-in-out duration-500 rounded-xl rounded-t-none px-4 py-8 w-full shadow-2xl text-center`}>
             {props.children}
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,14 @@ module.exports = {
         // Pantone: Forest Biome
         // https://www.pantone.com/color-finder/19-5230-TCX
         "forest-biome": "#194B46",
-
         // Pantone: Pink-a-boo
         // https://www.pantone.com/color-finder/13-2801-TCX
         "pink-a-boo": "#F7CDDB",
+        // A sky-like blue that I thought went well with the
+        // other colors
+        "blue-sky": "#BFDBFE",
+        // A deep gray that isn't black but is pretty close
+        "gray-deep": "#111827"
       }
     }
   },
